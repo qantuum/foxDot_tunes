25e91247b6e98253230a49dd835365550bbdbd84
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-19 12:22:38 +0100

    Create in_c.py

---
+++ in_c.py
@@ -0,0 +1,10 @@
+# Find the in C tune here : https://www.youtube.com/watch?v=yNi0bukYRnA
+
+# Now for the 53 different patterns, all converted in FoxDot language
+
+# First defines
+Clock.bpm = 120
+Root.default.set(0)
+Scale.default.set("major")
+
+# Pattern list :
