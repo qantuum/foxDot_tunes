17de7dc924b4ea73f3308b3b0ca3384da4454796
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-21 09:08:27 +0100

    Update durshaper.py

--- durshaper.py
+++ durshaper.py
@@ -1,8 +1,9 @@
-from random import randint
 def DurShaper(exp):
+    from random import randint
+    sr = random.SystemRandom()
     list = []
     indexes = random.randint(1,9)
     dividers = [1,2,4,8]
     for i in range(1,indexes):
-        list[i] = random.randint(1,8)
+        list.append(random.randint(1,8)/sr.choice(dividers))
     return list
