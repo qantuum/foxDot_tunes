e64743d795b9f327a8568af5c649ec50d50b2016
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-21 09:31:09 +0100

    Update durshaper.py

--- durshaper.py
+++ durshaper.py
@@ -1,4 +1,4 @@
-def DurShaper(exp):
+def DurShaper(target):
     from random import randint
     sr = random.SystemRandom()
     indexes = random.randint(1,exp*2)
@@ -6,9 +6,9 @@ def DurShaper(exp):
     list=[]
     for i in range(0,indexes):
         a = random.randint(1,8)/sr.choice(dividers)
-        if sum(list)+a < exp:
+        if sum(list)+a < target:
             list.append(a)
-    list.append(exp-sum(list))                   
-    return [list,sum(list)]
+    list.append(target-sum(list))                   
+    return list # always return a list of durations with total duration equals target
 
 a = DurShaper(4)
