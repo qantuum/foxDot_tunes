944b9d3394bc784c947d061cd8010a58030e752f
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-21 09:46:15 +0100

    Update durshaper.py

--- durshaper.py
+++ durshaper.py
@@ -1,11 +1,11 @@
 def DurShaper(target):
     from random import randint
     sr = random.SystemRandom()
-    indexes = random.randint(1,target*2)
-    dividers = [1,1,1,1,2,2,2,2,4,8] # 1/4 and 1/8-typed notes get more scarce
+    indexes = random.randint(1,target+4)
+    dividers = [1,1,1,2,2,2,2,4,8] # 1/4 and 1/8-typed notes get more scarce
     list=[]
     for i in range(0,indexes):
-        a = random.randint(1,target+2)/sr.choice(dividers)
+        a = random.randint(1,target)/sr.choice(dividers)
         if sum(list)+a < target:
             list.append(a)
     list.append(target-sum(list))                   
