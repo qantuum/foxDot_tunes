e1af75a00bd84ae04520e3bff108a7c3c2209bf0
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-21 09:27:04 +0100

    Update durshaper.py

--- durshaper.py
+++ durshaper.py
@@ -4,8 +4,11 @@ def DurShaper(exp):
     indexes = random.randint(1,exp*2)
     dividers = [1,2,4,8]
     list=[]
-    while sum(list) != exp:
-        list = []
-        for i in range(0,indexes):
-            list.append(random.randint(1,8)/sr.choice(dividers))
+    for i in range(0,indexes):
+        a = random.randint(1,8)/sr.choice(dividers)
+        if sum(list)+a < exp:
+            list.append(a)
+    list.append(exp-sum(list))                   
     return [list,sum(list)]
+
+a = DurShaper(4)
