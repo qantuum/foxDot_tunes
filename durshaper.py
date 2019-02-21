6b79537baba7ac33efe5d97c39d2bd774b4c4b32
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-21 13:04:42 +0100

    Update durshaper.py

--- durshaper.py
+++ durshaper.py
@@ -14,7 +14,7 @@ def DurShaper(target):
         if sum(list)+a < target:
             list.append(a)
     list.append(target-sum(list))                   
-    return list # always return a list of durations with total duration equals target
+    return [P[list].shuffle(),len(list)] # always return a list of durations with total duration equals target
 
 # variation giving shorter durations
 def DurShaperSm(target):
@@ -33,13 +33,14 @@ def DurShaperSm(target):
         if sum(list)+a/2 < target:
             list.append(a/2)
     list.append(target-sum(list))                   
-    return list # always return a list of durations with total duration equals target
+    return [P[list].shuffle(),len(list)] # always return a list of durations with total duration equals target
 
-# a variation with somewhat more score-like results
+# a variation with score-like durations
 def DurShaperSc(target):
     from random import randint
     sr = random.SystemRandom()
     indexes = random.randint(1,target+4)
+    standards = [0.25,0.375,0.75,0.5,1,2,3,4] # standard dur values found in scores
     dividers = [1,1,2] # skip 1/4 and 1/8 (comes after)
     list=[]
     for i in range(0,indexes):
@@ -56,17 +57,16 @@ def DurShaperSc(target):
         if sum(list)+a/4 < target:
             list.append(a/4)
     if sum(list) < target:
-        standards = [0.25,0.375,0.75,0.5,1,2,3,4] # standard dur values found in scores
         for i in range(0,len(standards)):
             if target-sum(list)%standards[i] != 0 and standards[i] < target-sum(list):
                 list.append(standards[i])
         if sum(list) < target:
-            list.append(target-sum(list))                                  
-    return list # always return a list of durations with total duration equals target
+            list.append(target-sum(list))                                 
+    return [P[list].shuffle(),len(list)] # always return a list of durations with total duration equals target
 
 # test it !
-a = DurShaper(8)
-b = DurShaperSm(4)
+a = DurShaper(8)[0]
+b = DurShaperSm(4)[0]
 print(a,b)
 Scale.default.set("minorPentatonic")
 aa >> pluck(P[[0,3,4],[0,2,1],[0,5,6],[0,7,8],[0,4],[0,3]].every(8,"shuffle"),dur=Pvar([a,b,PDur(3,8)],[8,4,4]))
@@ -75,11 +75,11 @@ ac >> prophet(aa.degree[0] + P(0,3,5),dur=4, oct=(5,5,4))
 op >> play("<x o ><->")
 
 # another example of setup using pattern methods :
-a = DurShaperSc(4)
+a = DurShaperSc(4)[0]
 print(a)
 Scale.default.set("mixolydian")
 Root.default.set(-3)
-aa >> pluck(PRand(5),dur= P[a] | P[a].every(6,"rotate",3) )    
+aa >> pluck(PRand(5),dur= a | a.every(6,"rotate",3) )    
 ab >> zap(aa.degree[1] + P[3,5], dur=1/2,drive=0.05)
 ac >> keys(aa.degree[0] + P(0,3,7),dur=2, oct=(6,5,5),formant=0.03)
 op >> play("<x o ><->")
