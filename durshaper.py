16bbdf08f24c6d8621551d68de2fe50ab2225f79
qantuum <36848803+qantuum@users.noreply.github.com>
2019-02-21 09:37:30 +0100

    Update durshaper.py

--- durshaper.py
+++ durshaper.py
@@ -2,7 +2,7 @@ def DurShaper(target):
     from random import randint
     sr = random.SystemRandom()
     indexes = random.randint(1,target*2)
-    dividers = [1,2,4,8]
+    dividers = [1,1,2,2,2,4,8] # grab more chances to have 1 and 1/2 typed durations
     list=[]
     for i in range(0,indexes):
         a = random.randint(1,8)/sr.choice(dividers)
